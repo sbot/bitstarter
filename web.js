812b61de17947036524ee00ec8b22dec498615d8
Mark Whelan <m@townunderground.net>
2013-07-08 18:09:18 +0000

    buffer test

--- web.js
+++ web.js
@@ -2,11 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buffer = new Buffer(fs.readFileSync('index.html'));
+var buffer = new Buffer("new buffer test");
 
 app.get('/', function(request, response) {
-  //response.send(buffer.toString);
-  response.send("testing");
+  response.send(buffer.toString);
+  //response.send("testing");
 });
 
 var port = process.env.PORT || 5000;
