9e87a4cf6926bfc8debaf843c743ca36088591c6
Mark Whelan <m@townunderground.net>
2013-07-08 18:30:20 +0000

    buffer test

--- web.js
+++ web.js
@@ -2,7 +2,8 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buffer = fs.readFileSync("index.html");
+var buffer = fs.readFileSync("./index.html");
+if err console.log(err);
 
 app.get('/', function(request, response) {
   response.send(buffer.toString('utf8'));
